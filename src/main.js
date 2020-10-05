1897839623cde87b1451095698a22650693e4112
wutangyuan <wutangyuan1@gmail.com>
2020-10-05 21:30:22 +0800

    提交文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+//根路径
+//Axios.defaults.baseURL="https://www.liulongbin.top:8888/api/private/v1/";
+
+Axios.defaults.baseURL="http://localhost:5000/api/";
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import '../assets/css/global.css'
+
+import Welcome from '../components/Welcome.vue' 
+
+import Users from '../components/user/Users.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
+   //路由重定向
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'/',
+    redirect:'/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+   path: '/login',
+   component : Login
+  },
+  {
+    path: '/home',
+    component : Home,
+    redirect:'/welcome',
+    children:
+    [
+      {
+        path:'/welcome',
+        component:Welcome
+      },
+      {
+        path:'/users',
+        component:Users
+      }
+    ]
+
+   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+//路由导航守卫
+router.beforeEach((to,from,next)=>{
+    if(to.path=='/login')
+    {
+      return next();
+    }
+   var token= window.sessionStorage.getItem("token");
+   if(!token)
+   {
+     return next('/login');
+   }
+   next();
+});
 
 export default router
